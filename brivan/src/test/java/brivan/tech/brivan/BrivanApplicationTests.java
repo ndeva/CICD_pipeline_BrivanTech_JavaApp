2b955ff7618db8824f7a77a942492866f3fb5ca6
Brian Ndeva Jumba <ndevabn@gmail.com>
2023-01-27 20:27:24 +0300

    created a contoller: Devices

---
+++ brivan/src/test/java/brivan/tech/brivan/BrivanApplicationTests.java
@@ -0,0 +1,13 @@
+package brivan.tech.brivan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrivanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
